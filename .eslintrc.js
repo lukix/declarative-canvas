de5538978d0a879907d64b7643cf3a607e5277d5
Łukasz Jenczmyk <lukix.95@gmail.com>
2020-09-19 18:44:45 +0200

    Adjust eslint config to work with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:jest/recommended'],
-  plugins: ['jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['jest', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
@@ -14,6 +20,9 @@ module.exports = {
     browser: true,
     'jest/globals': true,
   },
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
   root: true,
 };
